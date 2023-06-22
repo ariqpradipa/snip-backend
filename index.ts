d449595df87e7c1978a45445e6ac7b45e6a8abdd
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2023-06-22 22:23:37 +0700

    fix: default port usage

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import checkBarcode from './src/routes/checkBarcodeRoute';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
